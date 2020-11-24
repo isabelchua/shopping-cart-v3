60e230bfad1b02b391ea3184e8da10f9ff2f948f
isabelchua <isabelchua86@gmail.com>
2020-11-24 11:47:42 -0800

    redux store

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import cartItems from "./cart-items";
 // redux stuff
 import { createStore } from "redux";
 //reducer
-import { DECREASE, INCREASE } from "./actions";
+import { Provider } from "react-redux";
+//import { DECREASE, INCREASE } from "./actions";
 import reducer from "./reducer";
 
 const initialStore = {
-	count: 0,
-	name: "john"
+	cart: cartItems,
+	total: 0,
+	amount: 5
 };
 
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: DECREASE });
-// store.dispatch({ type: RANDOM });
-// store.dispatch({ type: CHANGE_NAME });
-// store.dispatch({ type: RESET });
-store.dispatch({ type: INCREASE });
-console.log(store.getState());
+// store.dispatch({ type: DECREASE });
+// // store.dispatch({ type: RANDOM });
+// // store.dispatch({ type: CHANGE_NAME });
+// // store.dispatch({ type: RESET });
+// store.dispatch({ type: INCREASE });
+// console.log(store.getState());
 
 function App() {
 	// cart setup
 
 	return (
-		<main>
-			<Navbar cart={store.getState()} />
+		<Provider store={store}>
+			<Navbar />
+			{/* <Navbar cart={store.getState()} /> */}
 			<CartContainer cart={cartItems} />
-		</main>
+		</Provider>
 	);
 }
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,14 @@
 import { DECREASE, INCREASE } from "./actions";
 
+// 53:49
 function reducer(state, action) {
-	console.log({ state, action });
-	if (action.type === DECREASE) {
-		return { ...state, count: state.count - 1 };
-	}
-	if (action.type === INCREASE) {
-		return { ...state, count: state.count + 1 };
-	}
+	// console.log({ state, action });
+	// if (action.type === DECREASE) {
+	// 	return { ...state, count: state.count - 1 };
+	// }
+	// if (action.type === INCREASE) {
+	// 	return { ...state, count: state.count + 1 };
+	// }
 	// if (action.type === RESET) {
 	// 	return { ...state, count: 0 };
 	// }
