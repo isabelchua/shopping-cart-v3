9c4e8f24c2583a02e5dea50c10aa6beabc9cb7bc
isabelchua <isabelchua86@gmail.com>
2020-11-23 16:43:02 -0800

    adding different dispatch

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ import { createStore } from "redux";
 //reducer
 
 const initialStore = {
-	count: 78
+	count: 0
 };
 
 function reducer(state, action) {
 	console.log({ state, action });
+	if (action.type === "DECREASE") {
+		return { count: state.count - 1 };
+	}
+	if (action.type === "INCREASE") {
+		return { count: state.count + 1 };
+	}
+	if (action.type === "RESET") {
+		return { count: 0 };
+	}
 	return state;
 }
 const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
 console.log(store.getState());
 
 function App() {
